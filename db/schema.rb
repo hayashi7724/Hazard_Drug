14aa3f00295d38f1659c2fc2a77f961e68e4229c
hayashi7724 <tetsuya7724@gmail.com>
2020-05-25 20:39:42 +0900

    js

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_104143) do
+ActiveRecord::Schema.define(version: 2020_05_19_113648) do
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
